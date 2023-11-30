827e94409cf09ec99c50c28f06ff613d327c46e5
jackholzhauerfromthatoneschool <142598257+jackholzhauerfromthatoneschool@users.noreply.github.com>
2023-11-30 17:27:43 -0600

    made it so in theory the program can access the file

--- TheInfinite/src/main.java
+++ TheInfinite/src/main.java
@@ -1,6 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        
+        try {
+        Scanner reader = new Scanner(new File("textfiles/gradetest.txt"));
+        System.out.println(reader.next());
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+      
+          }
     }
 }
