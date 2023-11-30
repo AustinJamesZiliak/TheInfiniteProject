1b7c40109a2f176211420009e0f8a201383174e9
jackholzhauerfromthatoneschool <142598257+jackholzhauerfromthatoneschool@users.noreply.github.com>
2023-11-29 19:49:00 -0600

    Added student object

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+/**
+ * Write a description of class main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class main//must be here
+{
+    public static void main(String[] args)
+    {
+            student dave=new student(80.0,5.0,2.0,2.0,2.0,2.0,3.0);
+    }
+}
