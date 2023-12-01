1faab41b1984f370159dbce37f2d80d422ca5aa9
AustinJamesZiliak <Ajziliak@stu.naperville203.org>
2023-11-30 20:01:16 -0600

    Added devlog and disproved VSCodes accuracy with file IO

--- TheInfinite/src/main.java
+++ TheInfinite/src/main.java
@@ -1,15 +1,15 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         System.out.println("Hello, World!");
         try {
-        Scanner reader = new Scanner(new File("Devlogs.txt"));
-        System.out.println(reader.next());
+        File gradesFile = new File("gradeTest.txt");
+        Scanner reader = new Scanner(gradesFile);
+            System.out.println(reader.next());
         }
-        catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
+        catch (java.io.FileNotFoundException e) {
+            System.out.println("Error: "+e+ "occurred.");
       
           }
     }
